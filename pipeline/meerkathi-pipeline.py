662ac8d4cc507bb49166d4b6e0f0030a5b376b3c
Kat user <kat@>
2017-05-16 11:22:58 +0200

    add converting h5toms step

--- pipeline/meerkathi-pipeline.py
+++ pipeline/meerkathi-pipeline.py
@@ -1,14 +1,40 @@
 import stimela
 
+INPUT = 'input'
+OUTPUT = 'output'
+MSDIR = 'msdir'
+
+h5file = '2017/04/06/1491463063.h5'
+msname = '1491463063.ms'
+prefix = 'meerkathi-1491463063'
+
+recipe = stimela.Recipe('MeerKATHI pipeline', ms_dir=MSDIR)
 
 
-print("""	
-  _    _           _              _____                _                   _   _             
- | |  | |         | |            / ____|              | |                 | | (_)            
- | |  | |_ __   __| | ___ _ __  | |     ___  _ __  ___| |_ _ __ _   _  ___| |_ _  ___  _ __  
- | |  | | '_ \ / _` |/ _ \ '__| | |    / _ \| '_ \/ __| __| '__| | | |/ __| __| |/ _ \| '_ \ 
- | |__| | | | | (_| |  __/ |    | |___| (_) | | | \__ \ |_| |  | |_| | (__| |_| | (_) | | | |
-  \____/|_| |_|\__,_|\___|_|     \_____\___/|_| |_|___/\__|_|   \__,_|\___|\__|_|\___/|_| |_|
-                                                                                             
-""")                                                                         
+recipe.add('cab/h5toms', 'h5toms', 
+    {
+        "hdf5files"    	:    h5file,
+        "channel-range"	:    "'20873,21639'",
+        "no-auto"       :    False,
+        "output-ms"     :    msname,
+        "full-pol"      :    True,
+    },
+    input='/var/kat/archive2/data/MeerKATAR1/telescope_products',
+    output=MSDIR,
+    label='h5toms:: Convert from h5 to MS')
+
+recipe.add('cab/casa_listobs', 'obsinfo', 
+    {
+        "vis"    	:    msname,
+        "listfile"      :    prefix+'-listobs.txt'
+    },
+    input=INPUT,
+    output=OUTPUT,
+    label='get_obsinfo:: Get observation information')
+
+
+recipe.run([
+#    'h5toms',
+    'get_obsinfo',
+])
 

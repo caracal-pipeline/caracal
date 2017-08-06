03195bd64f936184e905d3e7e0cbc3237c73505c
kat <kat@ska.ac.za>
2017-08-06 23:10:38 +0200

    set refernce antenna

--- meerkathi/workers/observation_config_worker.py
+++ meerkathi/workers/observation_config_worker.py
@@ -4,20 +4,20 @@ import meerkathi
 import sys
 
 NAME = 'Automatically catergorize observed fields'
-####TODO(Sphe): 
-# Log how long the fields were observed
-# Revamp set_model in crosscal_worker to check fcal in native in CASA database, then 
-# decide on best way to set model.
+
 def worker(pipeline, recipe, config):
-    "Requires *-obsinfo.json file from get_data worker"
+    """Requires *-obsinfo.json file from get_data worker"""
 
     # itnitialse things
-    pipeline.fcal = [None] * pipeline.nobs
-    pipeline.bpcal = [None] * pipeline.nobs
-    pipeline.gcal = [None] * pipeline.nobs
-    pipeline.target = [None] * pipeline.nobs
-    pipeline.reference_antenna = [None] * pipeline.nobs
-
+    for item in 'fcal bpcal gcal target reference_antenna'.split():
+        val = config[item]
+        if val and not isinstance(config[item], list):
+            setattr(pipeline, item, [config[item]]*pipeline.nobs)
+        elif isinstance(config[item], list):
+            setattr(pipeline, item, config[item])
+        else:
+            setattr(pipeline, item, [None]*pipeline.nobs)
+            
     for i, prefix in enumerate(pipeline.prefixes):
         msinfo = '{0:s}/{1:s}-obsinfo.json'.format(pipeline.output, prefix)
         intents = utils.categorize_fields(msinfo)
@@ -34,26 +34,42 @@ def worker(pipeline, recipe, config):
             meerkathi.log.info('Auto selecting gain calibrator as {:s}'.format(pipeline.gcal[i]))
         else:
             pipeline.gcal[i] = config['gcal']
-#       meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.gcal[i], tobs))
+        tobs = utils.field_observation_length(msinfo, pipeline.gcal[i])/60.0
+        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.gcal[i], tobs))
   
         # Set flux calibrator
         if config['fcal'] == 'auto':
-            pipeline.fcal[i] = utils.observed_longest(msinfo, fcals)
+            while len(fcals)>0:
+                fcal = utils.observed_longest(msinfo, fcals)
+                #if utils.find_in_casa_calibrators(msinfo, fcal) or utils.find_in_native_calibrators(msinfo, fcal):
+                if utils.find_in_casa_calibrators(msinfo, fcal):
+                    pipeline.fcal[i] = fcal
+                    break
+                fcals.remove(fcal) 
             meerkathi.log.info('Auto selecting flux calibrator as {:s}'.format(pipeline.fcal[i]))
         else:
             pipeline.fcal[i] = config['fcal']
-        meerkathi.log.info('Checking if flux calibrator is in our database or in the CASA NRAO database')
-        found = False
-        if 
+        tobs = utils.field_observation_length(msinfo, pipeline.fcal[i])/60.0
+        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.fcal[i], tobs))
         
         # Set bandpass calibrator
         if config['bpcal'] == 'auto':
             pipeline.bpcal[i] = utils.observed_longest(msinfo, bpcals)
             meerkathi.log.info('Auto selecting bandpass calibrator field as {:s}'.format(pipeline.bpcal[i]))
+        else:
+            pipeline.bpcal[i] = onfig['bpcal']
+        tobs = utils.field_observation_length(msinfo, pipeline.bpcal[i])/60.0
+        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.bpcal[i], tobs))
 
+        # Select target field(s)
         if config['target'] == 'auto':
             pipeline.target[i] = ','.join(targets)
             meerkathi.log.info('Auto selecting target field as {:s}'.format(pipeline.target[i]))
         else:
-            pipeline.target[i] = config['target']
+            targets = config['target'].split(',')
+            pipeline.target[i] = ','.join(targets)
+        meerkathi.log.info('Found {0:d} target fields {1:s}'.format(len(targets), pipeline.target[i]))
+        for target in targets:
+            tobs = utils.field_observation_length(msinfo, target)/60.0
+            meerkathi.log.info('Targer field "{0:s}" was observed for {1:.2f} minutes'.format(target, tobs))
 

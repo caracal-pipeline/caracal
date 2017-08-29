f0c5b9e533fdcc9d906cd1348851c2cfa59d0525
Benjamin Hugo <bennahugo@aol.com>
2017-08-29 17:57:57 +0200

    Update observation_config_worker.py
    Fixes #82

--- meerkathi/workers/observation_config_worker.py
+++ meerkathi/workers/observation_config_worker.py
@@ -26,10 +26,7 @@ def worker(pipeline, recipe, config):
             msmeta = '{0:s}/{1:s}.json'.format(pipeline.data_path, pipeline.dataid[i])
             pipeline.reference_antenna[i] = utils.meerkat_refant(msmeta)
             meerkathi.log.info('Auto selecting reference antenna as {:s}'.format(pipeline.reference_antenna[i]))
-
-        if 'auto' not in [config[item] for item in 'fcal bpcal gcal target nchans'.split()]:
-            continue
-
+        
         # Get channels in MS
         if config['nchans'] == 'auto':
             with open(msinfo, 'r') as stdr:
@@ -37,54 +34,56 @@ def worker(pipeline, recipe, config):
                 pipeline.nchans[i] = spw
             meerkathi.log.info('MS has {0:d} spectral windows, with NCHAN={1:s}'.format(len(spw), ','.join(map(str, spw))))
         
-        intents = utils.categorize_fields(msinfo)
-        # Get fields and their purposes
-        fcals = intents['fcal'][-1]
-        gcals = intents['gcal'][-1]
-        bpcals = intents['bpcal'][-1]
-        targets = intents['target'][-1]
-        
-        # Set gain calibrator
-        if config['gcal'] == 'auto':
-            pipeline.gcal[i] = utils.select_gcal(msinfo, targets, gcals, mode='nearest')
-            meerkathi.log.info('Auto selecting gain calibrator as {:s}'.format(pipeline.gcal[i]))
-        else:
-            pipeline.gcal[i] = config['gcal']
-        tobs = utils.field_observation_length(msinfo, pipeline.gcal[i])/60.0
-        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.gcal[i], tobs))
-  
-        # Set flux calibrator
-        if config['fcal'] == 'auto':
-            while len(fcals)>0:
-                fcal = utils.observed_longest(msinfo, fcals)
-                if utils.find_in_casa_calibrators(msinfo, fcal) or utils.find_in_native_calibrators(msinfo, fcal):
-                    pipeline.fcal[i] = fcal
-                    break
-                fcals.remove(fcal) 
-            meerkathi.log.info('Auto selecting flux calibrator as {:s}'.format(pipeline.fcal[i]))
-        else:
-            pipeline.fcal[i] = config['fcal']
-        tobs = utils.field_observation_length(msinfo, pipeline.fcal[i])/60.0
-        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.fcal[i], tobs))
-        
-        # Set bandpass calibrator
-        if config['bpcal'] == 'auto':
-            pipeline.bpcal[i] = utils.observed_longest(msinfo, bpcals)
-            meerkathi.log.info('Auto selecting bandpass calibrator field as {:s}'.format(pipeline.bpcal[i]))
-        else:
-            pipeline.bpcal[i] = config['bpcal']
-        tobs = utils.field_observation_length(msinfo, pipeline.bpcal[i])/60.0
-        meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.bpcal[i], tobs))
+        #Auto select some/all fields if user didn't manually override all of them
+        if 'auto' in [config[item] for item in 'fcal bpcal gcal target'.split()]:
+            intents = utils.categorize_fields(msinfo)
+            # Get fields and their purposes
+            fcals = intents['fcal'][-1]
+            gcals = intents['gcal'][-1]
+            bpcals = intents['bpcal'][-1]
+            targets = intents['target'][-1]
 
-        # Select target field(s)
-        if config['target'] == 'auto':
-            pipeline.target[i] = ','.join(targets)
-            meerkathi.log.info('Auto selecting target field as {:s}'.format(pipeline.target[i]))
-        else:
-            targets = config['target'].split(',')
-            pipeline.target[i] = ','.join(targets)
-        meerkathi.log.info('Found {0:d} target fields {1:s}'.format(len(targets), pipeline.target[i]))
-        for target in targets:
-            tobs = utils.field_observation_length(msinfo, target)/60.0
-            meerkathi.log.info('Targer field "{0:s}" was observed for {1:.2f} minutes'.format(target, tobs))
+            # Set gain calibrator
+            if config['gcal'] == 'auto':
+                pipeline.gcal[i] = utils.select_gcal(msinfo, targets, gcals, mode='nearest')
+                meerkathi.log.info('Auto selecting gain calibrator as {:s}'.format(pipeline.gcal[i]))
+            else:
+                pipeline.gcal[i] = config['gcal']
+            tobs = utils.field_observation_length(msinfo, pipeline.gcal[i])/60.0
+            meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.gcal[i], tobs))
+
+            # Set flux calibrator
+            if config['fcal'] == 'auto':
+                while len(fcals)>0:
+                    fcal = utils.observed_longest(msinfo, fcals)
+                    if utils.find_in_casa_calibrators(msinfo, fcal) or utils.find_in_native_calibrators(msinfo, fcal):
+                        pipeline.fcal[i] = fcal
+                        break
+                    fcals.remove(fcal) 
+                meerkathi.log.info('Auto selecting flux calibrator as {:s}'.format(pipeline.fcal[i]))
+            else:
+                pipeline.fcal[i] = config['fcal']
+            tobs = utils.field_observation_length(msinfo, pipeline.fcal[i])/60.0
+            meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.fcal[i], tobs))
+
+            # Set bandpass calibrator
+            if config['bpcal'] == 'auto':
+                pipeline.bpcal[i] = utils.observed_longest(msinfo, bpcals)
+                meerkathi.log.info('Auto selecting bandpass calibrator field as {:s}'.format(pipeline.bpcal[i]))
+            else:
+                pipeline.bpcal[i] = config['bpcal']
+            tobs = utils.field_observation_length(msinfo, pipeline.bpcal[i])/60.0
+            meerkathi.log.info('Field "{0:s}" was observed for {1:.2f} minutes'.format(pipeline.bpcal[i], tobs))
+
+            # Select target field(s)
+            if config['target'] == 'auto':
+                pipeline.target[i] = ','.join(targets)
+                meerkathi.log.info('Auto selecting target field as {:s}'.format(pipeline.target[i]))
+            else:
+                targets = config['target'].split(',')
+                pipeline.target[i] = ','.join(targets)
+            meerkathi.log.info('Found {0:d} target fields {1:s}'.format(len(targets), pipeline.target[i]))
+            for target in targets:
+                tobs = utils.field_observation_length(msinfo, target)/60.0
+                meerkathi.log.info('Targer field "{0:s}" was observed for {1:.2f} minutes'.format(target, tobs))
 

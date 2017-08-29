c55d2d12f929e5eec48c3e31ca33e13762e4d7ee
Benjamin Hugo <bennahugo@aol.com>
2017-08-29 11:16:15 +0200

    Update flagging_worker.py

--- meerkathi/workers/flagging_worker.py
+++ meerkathi/workers/flagging_worker.py
@@ -59,7 +59,7 @@ def worker(pipeline, recipe, config):
                     "msname"    : msname,
                     "mask"      : config['static_mask']['mask'],
                     "accumulation_mode" : 'or',
-                    "uvrange"   : config['static_mask'].get('uvrange', "0~10"),
+                    "uvrange"   : config['static_mask'].get('uvrange', "''"),
                     "memory"    : 4096,
                 },
                 input=pipeline.input,

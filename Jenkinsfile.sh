64a8929d9566d33e8e86ec808aaf51a2fa149af4
Fil8 <filippo.maccagni@gmail.com>
2018-10-22 14:44:00 +0200

    updated jenkinsfile with mvtoms instead h5toms

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -66,7 +66,7 @@ meerkathi --get_data_meerkat_query_available_poll_mode override \
           --general_data_path $TEST_DATA_DIR \
           --get_data_meerkat_query_available_enable no \
           --get_data_download_enable no \
-          --get_data_h5toms_channel_range 2525,2781 \
+          --get_data_mvtoms_channel_range 2525,2781 \
           --self_cal_img_npix 4096 \
           --self_cal_cal_niter 3 \
           --self_cal_image_enable yes \

f762361024dc07c2794e21986adb73f9760ad03f
Fil8 <filippo.maccagni@gmail.com>
2018-10-24 12:19:07 +0200

    merge positivity option not active in stimela, took it out from sofia masking

--- meerkathi/workers/self_cal_worker.py
+++ meerkathi/workers/self_cal_worker.py
@@ -260,8 +260,6 @@ def worker(pipeline, recipe, config):
               "merge.minSizeX"        : 3,
               "merge.minSizeY"        : 3, 
               "merge.minSizeZ"        : 1,
-              "merge.positivity"      : config[key].get('positivity',False),
-
             }
         if config[key].get('flag') :
           flags_sof = config[key].get('flagregion')

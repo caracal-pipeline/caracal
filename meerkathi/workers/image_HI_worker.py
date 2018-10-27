b54a7cf5723590f0cbd97adaceb4b0af42102e17
paoloserra <paolo.serra@inaf.it>
2018-10-27 10:23:13 +0200

    debugging attempt

--- meerkathi/workers/image_HI_worker.py
+++ meerkathi/workers/image_HI_worker.py
@@ -86,6 +86,7 @@ def worker(pipeline, recipe, config):
                    obsdate = datetime.datetime.strptime(obsstartdate, '%d-%b-%Y').strftime('%Y-%m-%d')
                    targetpos = SkyCoord(RA[i], Dec[i], frame='icrs', unit='deg')
                    meerkathi.log.info('Using astropy version {0:}'.format(astropy.__version__))
+                   meerkathi.log.info('Passing the following arguments to SkyCoord: {0:}, {1:}'.format(RA[i],Dec[i]))
                    meerkathi.log.info('Passing the following arguments to targetpos.radial_velocity_correction: {0:}, {1:}'.format(Time(obsdate),telloc))
                    v=targetpos.radial_velocity_correction(kind='barycentric',obstime=Time(obsdate), location=telloc).to('km/s')
                    meerkathi.log.info('Resulting v = {0:}'.format(v))

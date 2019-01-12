da91b77ce14e74abd7a73098670e934b36547238
bennahugo <bennahugo@aol.com>
2019-01-12 12:20:16 +0200

    Remove invalid argument from test script

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -52,6 +52,5 @@ meerkathi --get_data_dataid 1477074305 \
           --self_cal_highfreqres_contim_chans 9 \
           --self_cal_highfreqres_contim_deconv_chans 3 \
           --self_cal_highfreqres_contim_fit_spectral_pol 1 \
-          --no-interactive \
-          --disable-pdb-post-mortem
+          --no-interactive
 

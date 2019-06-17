f8ec7a776ce86bd1f6d86104f04d2bbcc91bc714
PeterKamphuis <peterkamphuisastronomy@gmail.com>
2019-06-17 16:01:09 +0200

    Revert to master flagging

--- meerkathi/workers/flagging_worker.py
+++ meerkathi/workers/flagging_worker.py
@@ -241,19 +241,9 @@ def worker(pipeline, recipe, config):
 
         if pipeline.enable_task(config, 'autoflag_rfi'):
             step = 'autoflag_{0:d}'.format(i)
-            # The allowance of zero in the field parameter is a temporary fix to Issue #308. Once proper field ID tracing is implemented this should be reverted to the following code:
-            #if config['autoflag_rfi'].get('fields', 'auto') != 'auto' and \
-            #   not set(config['autoflag_rfi'].get('fields', 'auto').split(',')) <= set(['gcal', 'bpcal', 'target', 'fcal']):
-            #    raise KeyError("autoflag rfi can only be 'auto' or be a combination of 'gcal', 'fcal', 'bpcal', or 'target'")
-            #if config['autoflag_rfi'].get('calibrator_fields', 'auto') != 'auto' and \
-            #   not set(config['autoflag_rfi'].get('calibrator_fields', 'auto').split(',')) <= set(['gcal', 'bpcal', 'fcal']):
-            #    raise KeyError("autoflag rfi fields can only be 'auto' or be a combination of 'gcal', 'bpcal', 'fcal'")
-            #def_fields = ','.join([pipeline.bpcal_id[i], pipeline.gcal_id[i]])
-            #fields = def_fields if config['autoflag_rfi'].get('fields', 'auto') == 'auto' else \
-            #         ",".join([getattr(pipeline, key + "_id")[i] for key in config['autoflag_rfi'].get('fields').split(',')])
             if config['autoflag_rfi'].get('fields', 'auto') != 'auto' and \
-               not set(config['autoflag_rfi'].get('fields', 'auto').split(',')) <= set(['gcal', 'bpcal', 'target', 'fcal','0']):
-                raise KeyError("autoflag rfi can only be 'auto' or be a combination of 'gcal', 'fcal', 'bpcal', '0' or 'target'")
+               not set(config['autoflag_rfi'].get('fields', 'auto').split(',')) <= set(['xcal', 'gcal', 'bpcal', 'target', 'fcal']):
+                raise KeyError("autoflag rfi can only be 'auto' or be a combination of 'xcal', 'gcal', 'fcal', 'bpcal' or 'target'")
             if config['autoflag_rfi'].get('calibrator_fields', 'auto') != 'auto' and \
                not set(config['autoflag_rfi'].get('calibrator_fields', 'auto').split(',')) <= set(['xcal', 'gcal', 'bpcal', 'fcal']):
                 raise KeyError("autoflag rfi fields can only be 'auto' or be a combination of 'xcal', 'gcal', 'bpcal', 'fcal'")

2bd80e71fa354287cc5f8a0c56f8c60c9ea953d1
PeterKamphuis <peterkamphuisastronomy@gmail.com>
2019-11-21 20:47:36 +0100

    Remove cal from variable

--- meerkathi/workers/self_cal_worker.py
+++ meerkathi/workers/self_cal_worker.py
@@ -57,7 +57,7 @@ def worker(pipeline, recipe, config):
     label = config['label']
     time_chunk = config.get('cal_time_chunk')
     freq_chunk = config.get('cal_freq_chunk')
-    cal_min_uvw = config.get('cal_minuvw_m')
+    min_uvw = config.get('minuvw_m')
     ncpu = config.get('ncpu')
     mfsprefix = ["", '-MFS'][int(nchans > 1)]
     cal_niter = config.get('cal_niter')
@@ -241,8 +241,8 @@ def worker(pipeline, recipe, config):
             "multiscale-scales": sdm.dismissable(config[key].get('multi_scale_scales')),
             "savesourcelist": True if config[key].get('niter', niter)>0 else False,
         }
-        if cal_min_uvw > 0:
-            image_opts.update({"minuvw-m": cal_min_uvw})
+        if min_uvw > 0:
+            image_opts.update({"minuvw-m": min_uvw})
 
         if config[key].get('mask_from_sky'):
             fitmask = config[key].get('fits_mask')[
@@ -1012,8 +1012,8 @@ def worker(pipeline, recipe, config):
                 "madmax-estimate": 'corr',
                 "log-boring": True,
             }
-            if cal_min_uvw > 0:
-                cubical_opts.update({"sol-min-bl": cal_min_uvw})
+            if min_uvw > 0:
+                cubical_opts.update({"sol-min-bl": min_uvw})
             if config[key].get('two_step', False) and ddsols_[0] != -1:
                 cubical_opts.update({
                     "g-update-type": gupdate,

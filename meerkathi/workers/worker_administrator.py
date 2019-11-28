5c1dc5099834f29777b473ca7d413bc5386630e2
Oleg Smirnov <osmirnov@gmail.com>
2019-11-28 22:08:02 +0200

    initialize standard notebooks based on general.init_notebooks setting

--- meerkathi/workers/worker_administrator.py
+++ meerkathi/workers/worker_administrator.py
@@ -9,6 +9,7 @@ import traceback
 from datetime import datetime
 import stimela
 import glob
+import shutil
 from meerkathi.dispatch_crew.config_parser import config_parser as cp
 import logging
 import traceback
@@ -208,6 +209,22 @@ class worker_administrator(object):
             f = "{0:s}/data/meerkat_files/{1:s}".format(pckgdir, _f)
             if not os.path.exists("{0:}/{1:s}".format(self.input, _f)):
                 subprocess.check_call(["cp", "-r", f, self.input])
+
+        # Copy standard notebooks
+        if self.config['general']['init_notebooks']:
+            nbdir = os.path.join(os.path.dirname(meerkathi.__file__), "notebooks")
+            for notebook in self.config['general']['init_notebooks']:
+                nbfile = notebook + ".ipynb"
+                nbsrc = os.path.join(nbdir, nbfile)
+                nbdest = os.path.join(self.output, nbfile)
+                if os.path.exists(nbsrc):
+                    if os.path.exists(nbdest):
+                        log.info("Standard notebook {} already exists, won't overwrite".format(nbdest))
+                    else:
+                        log.info("Creating standard notebook {}".format(nbdest))
+                        shutil.copyfile(nbsrc, nbdest)
+                else:
+                    log.error("Standard notebook {} does not exist".format(nbsrc))
 
     def enable_task(self, config, task):
         a = config.get(task)

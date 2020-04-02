80db1ee1834279f2b7c31d1ceff57ce13dfdd6bc
Fil8 <filippo.maccagni@gmail.com>
2020-04-02 10:54:12 +0200

    add print

--- meerkathi/workers/self_cal_worker.py
+++ meerkathi/workers/self_cal_worker.py
@@ -1629,7 +1629,7 @@ def worker(pipeline, recipe, config):
                 if pipeline.enable_task(config, 'image'):
                     image(self_cal_iter_counter, get_dir_path(
                         image_path, pipeline), mslist, field)
-                if mask_key=='sofia' and self_cal_iter_counter != cal_niter:
+                if mask_key=='sofia' and self_cal_iter_counter != cal_niter+1:
                     sofia_mask(self_cal_iter_counter, get_dir_path(
                         image_path, pipeline), field)
                 if pipeline.enable_task(config, 'extract_sources'):

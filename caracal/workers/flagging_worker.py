728fb85de90f0a355ccad1fd25f4f60ed762d338
PeterKamphuis <peterkamphuisastronomy@gmail.com>
2020-04-23 15:03:23 +0200

    Unhijacking TSC

--- caracal/workers/flagging_worker.py
+++ caracal/workers/flagging_worker.py
@@ -439,7 +439,6 @@ def worker(pipeline, recipe, config):
                                    "field-names": fields,
                                    "flagging-strategy": 'polarisation',
                                    "config" : tricolour_strat,
-                                   "nworkers": config['flag_rfi']['tricolour'].get('ncpu'),
                                },
                                input=pipeline.input,
                                output=pipeline.output,

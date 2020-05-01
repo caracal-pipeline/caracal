5511b8729b0dd53ccf2f3b56d479026bceb1359b
Oleg Smirnov <osmirnov@gmail.com>
2020-05-01 19:26:28 +0200

    added new shadems plots and 'extremely controversial' syntax to inspect worker

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
     'ruamel.yaml',
     'decorator',
     'numpy',
-    'stimela>=1.4.1',
+    'stimela @ git+https://github.com/ratt-ru/Stimela@shadems-direct-cab',
     'scipy',
     'pysolr',
     'progressbar2',

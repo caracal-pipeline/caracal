19b132c39eb6ada8623e5829bec676d387965d40
gigjozsa <gigjozsast@gmail.com>
2020-06-02 18:42:54 +0200

    Added Jenkinstest

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -31,7 +31,11 @@ source $WORKSPACE/$BUILD_NUMBER/$PULLFOLDER/caratekit.sh -ws ${caracal_tests} \
                                                          -lc ${caracal_version} \
                                                          -ct ${pull_request_name} \
                                                          -dm \
+							 -sm \
                                                          -or \
                                                          -f \
                                                          -op \
+							 -saw \
+							 -um
+
 rm -rf $TEST_OUTPUT_DIR/$pull_request_name/stimela-singularity

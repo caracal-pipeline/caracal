641eaf058a6f7f46e2f423b502d40f906d2421a0
gigjozsa <gigjozsast@gmail.com>
2020-07-03 18:32:50 +0200

    Doing one R/O Jenkins test only

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -25,29 +25,17 @@ pull_request_data=$TEST_OUTPUT_DIR/pull_request_data/
 export pull_request_data
 pull_request_name=$(cd $PULLFOLDER; git rev-parse HEAD | sed 's/\(^.\{1,7\}\).*/\1/')
 export pull_request_name
-mkdir -p ${pull_request_name}_singularity
-mkdir -p ${pull_request_name}_docker
+mkdir -p ${pull_request_name}
 source $WORKSPACE/$BUILD_NUMBER/$PULLFOLDER/caratekit.sh -ws ${caracal_tests} \
                                                          -td ${pull_request_data} \
                                                          -lc ${caracal_version} \
-                                                         -ct ${pull_request_name}_singularity \
+                                                         -ct ${pull_request_name} \
 							 -sm \
+							 -dm \
                                                          -or \
                                                          -f \
                                                          -op \
 							 -ro \
 							 -spf $WORKSPACE/singularity_pullfolder \
 							 -hf $WORKSPACE/home \
-                                                         -hn ; \
-source $WORKSPACE/$BUILD_NUMBER/$PULLFOLDER/caratekit.sh -ws ${caracal_tests} \
-                                                         -td ${pull_request_data} \
-                                                         -lc ${caracal_version} \
-                                                         -ct ${pull_request_name}_docker \
-							 -dm \
-                                                         -or \
-                                                         -f \
-                                                         -op \
-							 -spf $WORKSPACE/singularity_pullfolder \
-							 -hf $WORKSPACE/home \
                                                          -hn
-

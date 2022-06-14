eca986e573e80c4e2a2ebb45dc7158d7775f1486
Fil8 <filippo.maccagni@gmail.com>
2022-06-14 11:12:07 +0200

    add stuff

--- caracal/workers/utils/flag_Uzeros.py
+++ caracal/workers/utils/flag_Uzeros.py
@@ -136,7 +136,7 @@ def convToStokesI(data,flags):
     return data, flags
 
 
-def makeCube(inVis,outCubePrefix,chanMin,chanMax,taper,robust,kind='scan'):
+def makeCube(inVis,outCubePrefix,chanMin,chanMax,taper,robust,imsize,cell,kind='scan'):
 
     # print(inVis,outCubePrefix)
 

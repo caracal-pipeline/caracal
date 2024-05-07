77e2a80e46952a25a927c36affbd0c8f7ef2ba20
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2024-05-08 01:37:01 +0200

    Overwrite existing region

--- caracal/workers/ddcal_worker.py
+++ caracal/workers/ddcal_worker.py
@@ -269,7 +269,7 @@ def worker(pipeline, recipe, config):
                 reg.append(region_dd)
             regfile = "de-{0:s}.reg".format(field)
             ds9_file = os.path.join(OUTPUT, outdir, regfile)
-            Regions(reg).write(ds9_file, format='ds9')
+            Regions(reg).write(ds9_file, format='ds9', overwrite=True)
 
     def dd_calibrate(field, mslist):
         key = 'calibrate_dd'

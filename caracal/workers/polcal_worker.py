07bb3111a74884ce52f957fa954e33dc1d7a8b27
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-07-16 10:25:45 +0200

    Set model for calibrator

--- caracal/workers/polcal_worker.py
+++ caracal/workers/polcal_worker.py
@@ -129,21 +129,40 @@ def xcal_model_fcal_leak(msname, msinfo, prefix_msbase, recipe, config, pipeline
                        output=pipeline.output,
                        label='{0:s}:: Set jansky ms={1:s}'.format(step, msname))
 
-        recipe.add("cab/casa_setjy", "set_model_%d" % 0,
-                   {
-                       "msname": msname,
-                       "usescratch": True,
-                       "field": field,
-                       "standard": "manual",
-                       "fluxdensity": polarized_calibrators[field]["fluxdensity"],
-                       "spix": polarized_calibrators[field]["spix"],
-                       "reffreq": polarized_calibrators[field]["reffreq"],
-                       "polindex": polarized_calibrators[field]["polindex"],
-                       "polangle": polarized_calibrators[field]["polangle"],
-                       "rotmeas": polarized_calibrators[field]["rotmeas"],
-                   },
-                   input=pipeline.input, output=pipeline.output,
-                   label="set_model_%d" % 0)
+        if not usedelay:
+            recipe.add("cab/casa_setjy", "set_model_%d" % 0,
+                       {
+                           "msname": msname,
+                           "usescratch": True,
+                           "field": field,
+                           "standard": "manual",
+                           "fluxdensity": polarized_calibrators[field]["fluxdensity"],
+                           "spix": polarized_calibrators[field]["spix"],
+                           "reffreq": polarized_calibrators[field]["reffreq"],
+                           "polindex": polarized_calibrators[field]["polindex"],
+                           "polangle": polarized_calibrators[field]["polangle"],
+                           "rotmeas": polarized_calibrators[field]["rotmeas"],
+                       },
+                       input=pipeline.input, output=pipeline.output,
+                       label="set_model_%d" % 0)
+        else:
+            modelpoint = utils.find_in_native_calibrators(msinfo, field, mode='mod')
+            opts = {
+                "vis": msname,
+                "field": field,
+                "standard": "manual",
+                "fluxdensity": modelpoint['I'],
+                "reffreq": '{0:f}GHz'.format(modelpoint['ref'] / 1e9),
+                "spix": [modelpoint[a] for a in 'abcd'],
+                "scalebychan": True,
+                "usescratch": True,
+            }
+            step = 'set_noise_diode_model_cal-{0:d}'.format(i)
+            cabtouse = 'cab/casa_setjy'
+            recipe.add(cabtouse, step, opts,
+                       input=pipeline.input,
+                       output=pipeline.output,
+                       label='{0:s}:: Set jansky ms={1:s}'.format(step, msname))
 
         gain_opts = {
             "vis": msname,
